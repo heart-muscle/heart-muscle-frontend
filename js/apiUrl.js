671d2d7124cbdd3d8089469f11eadd5ddc3278dc
JungDaeSuh <78460559+JungDaeSuh@users.noreply.github.com>
2021-12-31 16:28:02 +0900

    apiUrl AWS 서버로 수정

--- js/apiUrl.js
+++ js/apiUrl.js
@@ -1,3 +1,3 @@
 // let apiUrl = "http://heartmuscle-env.eba-v2mbdfk2.ap-northeast-2.elasticbeanstalk.com"
-let apiUrl = "http://localhost:8080"
-// let apiUrl = "https://www.heartmusclebackend.shop"
+// let apiUrl = "http://localhost:8080"
+let apiUrl = "https://www.heartmusclebackend.shop"
